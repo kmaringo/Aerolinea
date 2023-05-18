9f06ffc346c094d816be111d2c819e5a1d08c96f
kmaringo <johanna1745@hotmail.com>
2023-05-17 19:23:23 -0500

    se crea carpeta hbs

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require ('express');
 const app = express();
-const port = 8285
+const port = 8280
 const path = require('path')
 const hbs = require('hbs');
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'hbs/public')));
 
-app.set('views', path.join(__dirname+ '/public/views'));
+app.set('views', path.join(__dirname+ '/hbs/public/views'));
 app.set('view engine', 'hbs');
 
-hbs.registerPartials(__dirname + '/public/partials');
+hbs.registerPartials(__dirname + '/hbs/public/views/partials');
 
 app.get('/', (req, res) =>{
     res.render('disponibilidad')
